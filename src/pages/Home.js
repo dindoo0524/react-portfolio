1a7527a005795e516a259dfd4e61b696ea54a203
JinjuNa <48050357+JinjuNa@users.noreply.github.com>
2020-12-29 21:36:30 +0900

    Home.js 작업

---
+++ src/pages/Home.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import CardComponent from '../components/CardComponent';
+import './Home.css';
+import Slider from '../components/Slider';
+import workData1 from '../data/workData1.json';
+import workData2 from '../data/workData2.json';
+import workData3 from '../data/workData3.json';
+import toyData1 from '../data/toyData1.json';
+import toyData2 from '../data/toyData2.json';
+
+const Home = () => {
+
+  return (
+    <main>
+        <Slider />
+        <section className="content-section">
+          <CardComponent contentData={workData1} />
+          <CardComponent contentData={workData2} />
+          <CardComponent contentData={workData3} />
+          <CardComponent contentData={toyData1} />
+          <CardComponent contentData={toyData2} />
+          {/* <CardComponent contentData={cardContent2} /> */}
+        </section>
+    </main>
+  )
+}
+
+export default Home;
