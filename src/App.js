3821104f41a852025983b33ffdeb35ec1f32c563
JinjuNa <48050357+JinjuNa@users.noreply.github.com>
2020-12-29 23:02:19 +0900

    매인베너 작업

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import './Common.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import Header from './components/Header';
+// import Header from './components/Header';
 import Home from './pages/Home';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      {/* <Header /> */}
       <Router>
       <Switch>
           <Route exact path="/portfolio" component={Home}/>
